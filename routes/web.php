ed44cf793f7d67bd49f614b46ce2c978698719b0
alextalosro <alex.talos@gmail.com>
2022-02-15 12:30:32 +0200

    register_user_page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+    use App\Http\Controllers\RegisterController;
     use App\Models\Post;
     use App\Models\User;
     use Illuminate\Support\Facades\Route;
@@ -9,4 +10,8 @@
     Route::get('/', [PostController::class, 'index'])->name('home');
 
     Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+    Route::get('register', [RegisterController::class, 'create']);
+
+    Route::post('register', [RegisterController::class, 'store']);
 
