4ab56f6b76fed02f1f7908fa58284f8b286d5764
alextalosro <alex.talos@gmail.com>
2022-02-16 11:11:46 +0200

    markup for post-comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,23 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl"/>
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 month ago</time>
+            </p>
+        </header>
+
+        <p>
+            It is a long established fact that a reader will be distracted by the readable content
+            of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+            more-or-less normal distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English.
+        </p>
+    </div>
+</article>
