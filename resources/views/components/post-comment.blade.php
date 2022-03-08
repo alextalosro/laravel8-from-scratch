e73f5da638ac67af901b43de8e1059439b078de3
alextalosro <alex.talos@gmail.com>
2022-03-08 16:29:33 +0200

    working comments

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,23 +1,24 @@
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+@props(['comment'])
 
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl"/>
-    </div>
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
-            <p class="text-xs">
-                Posted
-                <time>8 month ago</time>
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/60?u={{ $comment->user_id }}" alt="" width="60" height="60" class="rounded-xl"/>
+        </div>
+
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
+                <p class="text-xs">
+                    Posted
+                    <time>{{ $comment->created_at->diffForHumans() }}</time>
+                </p>
+            </header>
+
+            <p>
+                {{ $comment->body }}
             </p>
-        </header>
-
-        <p>
-            It is a long established fact that a reader will be distracted by the readable content
-            of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
-            more-or-less normal distribution of letters, as opposed to using 'Content here, content
-            here', making it look like readable English.
-        </p>
-    </div>
-</article>
+        </div>
+    </article>
+</x-panel>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+    use App\Http\Controllers\PostCommentsController;
     use App\Http\Controllers\RegisterController;
     use App\Http\Controllers\SessionsController;
     use Illuminate\Support\Facades\Route;
@@ -9,7 +10,7 @@
 
     Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-
+    Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
     Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 
